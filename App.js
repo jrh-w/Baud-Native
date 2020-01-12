ddeb3221abf6a56da066f5536aa71fe5721673e5
jrh-w <jakub.r.wlodarz@gmail.com>
2020-01-12 14:01:42 +0100

    App.js modified

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Examples } from '@shoutem/ui';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
-export default function App() {
+/*export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello World!</Text>
     </View>
   );
-}
+}*/
 
-const styles = StyleSheet.create({
+/*const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+});*/
+
+const RootStack = createStackNavigator(
+  {
+    Menu: Menu,
+    Login: Login
+  },
+  {
+    initialRouteName: 'Login'
+  }
+);
+
+const AppContainer = createAppContainer(RootStack);
+
+export default class App extends React.Component {
+  render() {
+    return <AppContainer />;
+  }
+}
---
+++ components/MainComponent.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class Main extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Main Menu</Text>
+      </View>
+    );
+  }
+}
