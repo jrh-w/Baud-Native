d997d906b955144c4b6959665b1753b88ad6a747
dvxi <sim28888@gmail.com>
2020-04-10 13:29:18 +0200

    ddd

--- App.js
+++ App.js
@@ -20,15 +20,14 @@ import Login from "./components/LoginComponent";
   );
 }*/
 
-  async componentDidMount() {
-    await Font.loadAsync({
-      Montserrat_Regular: require('./assets/fonts/Montserrat/Montserrat_Regular.ttf'),
-      Montserrat_Bold: require('./assets/fonts/Montserrat/Montserrat_Bold.ttf'),
-      SemiBold: require('./assets/fonts/Montserrat/Montserrat-SemiBold.ttf'),
-      ...Ionicons.font,
-    });
-    this.setState({ isReady: true });
-  }
+/*const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});*/
 
 const RootStack = createStackNavigator(
   {
@@ -42,62 +41,8 @@ const RootStack = createStackNavigator(
 
 const AppContainer = createAppContainer(RootStack);
 
-    return (
-    <StyleProvider style={getTheme(material)}>
-      <Container style={{ backgroundColor: '#F7F7F7' }}>
-      <Header transparent>
-        <StatusBar barStyle="dark-content" />
-      </Header>
-      <Card style={{ marginTop: -100, paddingTop: 80 }}>
-        <CardItem>
-          <Body style={{ alignItems: 'center' }}>
-            <Row style={{ height: 225 }}>
-              <Image style={{ width: 200, height: 200 }} source={require('./assets/logo.png')} />
-            </Row>
-            <Row style={{ height: 50 }}>
-              <H1>
-                Baud
-              </H1>
-            </Row>
-          </Body>
-        </CardItem>
-      </Card>
-      <Content style={{ bounces: 'false' }}>
-          <Grid>
-            <Body>
-              <Col style={{ width: 300 }}>
-                <Body>
-                  <Row style={{ marginVertical: 20 }}>
-                    <H1>
-                    Log in
-                    </H1>
-                  </Row>
-                  <Item rounded>
-                    <Input style={{ paddingLeft: 15 }} placeholder='Username or email'/>
-                  </Item>
-                  <Item rounded>
-                    <Input style={{ paddingLeft: 15 }} secureTextEntry={true} placeholder='Password'/>
-                  </Item>
-                  <Button transparent>
-                    <Text>Forgot your password?</Text>
-                  </Button>
-                  <Row style={{ marginVertical: 20 }}>
-                    <Button style={{ width: 50 }} bordered large rounded>
-                      <Icon type='Entypo' name='chevron-right' />
-                    </Button>
-                  </Row>
-                  <Row style={{ marginTop: 20 }}>
-                    <Button style={{ width: 150 }} bordered large rounded>
-                      <Text style={{ marginLeft: 'auto', marginRight: 'auto' }}>Sign Up</Text>
-                    </Button>
-                  </Row>
-                </Body>
-              </Col>
-            </Body>
-          </Grid>
-        </Content>
-      </Container>
-    </StyleProvider>
-    );
+export default class App extends React.Component {
+  render() {
+    return <AppContainer />;
   }
 }
