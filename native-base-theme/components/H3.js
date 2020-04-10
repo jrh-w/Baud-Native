48e09fff246c6a6ab0eb7fe4bcdc0834668e97f0
dvxi <sim28888@gmail.com>
2020-04-10 12:23:04 +0200

    zmiana

--- native-base-theme/components/H3.js
+++ native-base-theme/components/H3.js
@@ -6,7 +6,10 @@ export default (variables /* : * */ = variable) => {
   const h3Theme = {
     color: variables.textColor,
     fontSize: variables.fontSizeH3,
-    lineHeight: variables.lineHeightH3
+    lineHeight: variables.lineHeightH3,
+    color: '#9C9C9C',
+    fontSize: 13,
+    fontWeight: 'bold'
   };
 
   return h3Theme;
