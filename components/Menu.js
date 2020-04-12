cc0de296c135cf4d8f03ea67b9b270010c3555eb
jrh-w <jakub.r.wlodarz@gmail.com>
2020-04-12 12:57:48 +0200

    Added menuButton

--- components/Menu.js
+++ components/Menu.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
+import MenuButton from './MenuButton';
 
 class Menu extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      icons: []
+    };
+  }
+
   render() {
+
+    this.buttons = this.state.icons.map((item, key) =>
+      <MenuButton icon={item.icon} navigation={item.navigation} name={item.name} active={item.active}/>
+    );
+
     return(
-      <View></View>
+      <View>
+        {this.buttons}
+      </View>
     );
   }
 }
