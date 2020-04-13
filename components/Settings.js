99ff67fe3d3296d1b54ff9ba3015ccf0da318f37
dvxi <sim28888@gmail.com>
2020-04-13 13:14:45 +0200

    ds

--- components/Settings.js
+++ components/Settings.js
@@ -4,8 +4,10 @@ import { Text, StyleProvider, Container, Content, Header } from 'native-base';
 
 import getTheme from '../native-base-theme/components';
 import material from '../native-base-theme/variables/material';
+import { ScrollView } from 'react-native';
 
 import Menu from './sub_components/Menu';
+import Certificates from './sub_components/Certificates';
 
 class Settings extends Component {
   constructor(props) {
@@ -16,9 +18,9 @@ class Settings extends Component {
     return(
       <StyleProvider style={getTheme(material)}>
         <Container>
-          <Content>
+          <Content style={{ backgroundColor: '#f4d322' }}>
+            <Certificates navigation={this.props.navigation}/>
           </Content>
-          <Menu navigation={this.props.navigation}/>
         </Container>
       </StyleProvider>
     );
