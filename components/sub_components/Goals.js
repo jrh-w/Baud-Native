c0451b2df012d50113e91d2b7299be807055dd80
jrh-w <jakub.r.wlodarz@gmail.com>
2020-04-19 12:13:55 +0200

    Working example of chart kit

--- components/sub_components/Goals.js
+++ components/sub_components/Goals.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 
 import { Card, CardItem, Body, H1, Text, Left } from 'native-base';
 import { Col, Row, Grid } from 'react-native-easy-grid';
-import { Image } from 'react-native';
+import { Image, StyleSheet, View, Dimensions } from 'react-native';
+
+import { LineChart } from "react-native-chart-kit";
 
 class Goals extends Component {
   constructor(props) {
@@ -11,22 +13,42 @@ class Goals extends Component {
 
   render() {
     return(
-      <Card>
-        <CardItem>
-          <Col>
-            <Row style={{ marginVertical: 5 }}>
-              <H1>
-                Your goal
-              </H1>
-            </Row>
-            <Row style={{ marginVertical: 5 }}>
-              <Text>
-                tutaj bedzie wykres jak go zrobimy
-              </Text>
-            </Row>
-          </Col>
-        </CardItem>
-      </Card>
+      <View>
+      <LineChart
+data={{
+  labels: [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+  ],
+  datasets: [
+    {
+      data: [20, 45, 28, 80, 99, 43],
+      strokeWidth: 2,
+    },
+  ],
+}}
+width={Dimensions.get('window').width - 16}
+height={220}
+chartConfig={{
+  backgroundColor: '#1cc910',
+  backgroundGradientFrom: '#eff3ff',
+  backgroundGradientTo: '#efefef',
+  decimalPlaces: 2,
+  color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+  style: {
+    borderRadius: 16,
+  },
+}}
+style={{
+  marginVertical: 8,
+  borderRadius: 16,
+}}
+/>
+      </View>
     );
   }
 }
