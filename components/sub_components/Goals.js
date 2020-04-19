0911a72d505c402106faeac193f92fd523790ff2
jrh-w <jakub.r.wlodarz@gmail.com>
2020-04-19 13:00:43 +0200

    Connected chart to Redux

--- components/sub_components/Goals.js
+++ components/sub_components/Goals.js
@@ -3,34 +3,45 @@ import React, { Component } from 'react';
 import { Card, CardItem, Body, H1, Text, Left } from 'native-base';
 import { Col, Row, Grid } from 'react-native-easy-grid';
 import { Image, StyleSheet, View, Dimensions } from 'react-native';
+import { connect } from 'react-redux';
 
 import { LineChart } from "react-native-chart-kit";
+
+const mapStateToProps = state => {
+  return {
+    userStats: state.userStats,
+  };
+};
 
 class Goals extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      userStats: {},
+    };
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    if(props !== state) {
+      return {
+        userStats: props.userStats,
+      };
+    }
+
+    return null;
   }
 
   render() {
+
+    const data = {
+      labels: this.state.userStats.labels,
+      datasets: this.state.userStats.datasets
+    };
+
     return(
       <View>
       <LineChart
-data={{
-  labels: [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-  ],
-  datasets: [
-    {
-      data: [20, 45, 28, 80, 99, 43],
-      strokeWidth: 2,
-    },
-  ],
-}}
+data={data}
 width={Dimensions.get('window').width - 16}
 height={220}
 chartConfig={{
@@ -53,4 +64,4 @@ style={{
   }
 }
 
-export default Goals;
+export default connect(mapStateToProps)(Goals);
