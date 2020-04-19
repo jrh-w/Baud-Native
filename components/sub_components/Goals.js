9a8025d40f5dec5b848a05e0c1aa9ae7ca8c9e1e
dvxi <sim28888@gmail.com>
2020-04-19 12:21:17 +0200

    dziala

--- components/sub_components/Goals.js
+++ components/sub_components/Goals.js
@@ -4,8 +4,6 @@ import { Card, CardItem, Body, H1, Text, Left } from 'native-base';
 import { Col, Row, Grid } from 'react-native-easy-grid';
 import { Image, StyleSheet, View, Dimensions } from 'react-native';
 
-import { LineChart } from "react-native-chart-kit";
-
 import {
   LineChart,
   BarChart,
@@ -15,8 +13,6 @@ import {
   StackedBarChart
 } from 'react-native-chart-kit';
 
-import { Dimensions } from 'react-native';
-
 class Goals extends Component {
   constructor(props) {
     super(props);
@@ -24,7 +20,6 @@ class Goals extends Component {
 
   render() {
     return(
-<<<<<<< HEAD
       <Card>
         <CardItem>
           <Col>
@@ -61,7 +56,6 @@ class Goals extends Component {
                   borderRadius: 16
                 }
               }}
-              bezier
               style={{
                 marginVertical: 8,
                 borderRadius: 16
@@ -71,44 +65,6 @@ class Goals extends Component {
           </Col>
         </CardItem>
       </Card>
-=======
-      <View>
-      <LineChart
-data={{
-  labels: [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-  ],
-  datasets: [
-    {
-      data: [20, 45, 28, 80, 99, 43],
-      strokeWidth: 2,
-    },
-  ],
-}}
-width={Dimensions.get('window').width - 16}
-height={220}
-chartConfig={{
-  backgroundColor: '#1cc910',
-  backgroundGradientFrom: '#eff3ff',
-  backgroundGradientTo: '#efefef',
-  decimalPlaces: 2,
-  color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
-  style: {
-    borderRadius: 16,
-  },
-}}
-style={{
-  marginVertical: 8,
-  borderRadius: 16,
-}}
-/>
-      </View>
->>>>>>> 89b3c90fbd9b1ba22c037df7428642c56fb45563
     );
   }
 }
